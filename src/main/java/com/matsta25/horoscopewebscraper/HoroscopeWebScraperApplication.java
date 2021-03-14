28ac0a535cbe535f444265014a98023e1aa94bd5
Mateusz Stańczak <mateusz.stanczak@pgf.com.pl>
2021-03-14 19:47:33 +0100

    feat(app): AIO

---
+++ src/main/java/com/matsta25/horoscopewebscraper/HoroscopeWebScraperApplication.java
@@ -0,0 +1,13 @@
+package com.matsta25.horoscopewebscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoroscopeWebScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoroscopeWebScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matsta25/horoscopewebscraper/HoroscopeWebScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsta25.horoscopewebscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoroscopeWebScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
