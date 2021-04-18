a82f7d51751de13eb0f3ec8d8158ad4ea77c6457
Mateusz Stańczak <mateusz.stanczak@pgf.com.pl>
2021-04-19 00:16:49 +0200

    feat(app): change build-build-push.sh

--- build-build-push.sh
+++ build-build-push.sh
@@ -2,5 +2,5 @@
 
 ./gradlew :spotlessApply -Dorg.gradle.java.home=/c/Program\ Files/Java/jdk-11.0.1/ &&
 ./gradlew build -Dorg.gradle.java.home=/c/Program\ Files/Java/jdk-11.0.1/ &&
-docker build -t matsta25.tk:5000/horoscope-web-scraper . &&
-docker push matsta25.tk:5000/horoscope-web-scraper
+docker build -t 34.118.120.110:5000/horoscope-web-scraper . &&
+docker push 34.118.120.110:5000/horoscope-web-scraper
